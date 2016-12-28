b10e7ed1932eeb2dd2416410a4b54455e5c3f9c3
Sami <sami7mason@yahoo.co.uk>
2016-12-28 21:48:31 +0000

    Chapter5 added

---
+++ Chapter5/biggerandbetter.rb
@@ -0,0 +1,4 @@
+puts 'What is your favourite number?'
+favnumber = gets.chomp.to_i
+bignumber = favnumber + 1
+puts bignumber.to_s + ' is a much bigger and better number.'
---
+++ Chapter5/namegreeter.rb
@@ -0,0 +1,7 @@
+puts 'Hi there, what is your first name?'
+firstname = gets.chomp
+puts 'and your middle name?'
+midname = gets.chomp
+puts 'and finally, your last name?'
+lastname = gets.chomp
+puts "Hello, " + firstname + ' ' + midname + ' ' + lastname + '.'
